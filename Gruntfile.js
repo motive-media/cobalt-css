4d0d2d46cf60ff3d78895b2a1ef4f6a93175614b
Will Ayers <willayers.net@gmail.com>
2013-09-13 13:38:20 -0600

    Set up grunt, added default buttons to css and others to get it started.

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("bower.json"),
+
+        less: {
+            dev: {
+                files: {
+                    "cobalt.css": "cobalt.less"
+                }
+            },
+            production: {
+                options: {
+                    yuicompress: true
+                },
+                files: {
+                    "cobalt.min.css": "cobalt.less"
+                }
+            }
+        },
+
+        watch: {
+            build: {
+                files: ['cobalt.less', 'less/**/*.less'],
+                tasks: ['less']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+};
